da5e3caef24d31ab9164499babf3b90d611e9ea3
KLU <2400030533@kluniversity.in>
2026-04-28 13:57:23 +0530

    feat: add VITE_API_URL env variable for deployment, add .env.example

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:2007",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:2007",
   headers: { "Content-Type": "application/json" },
 });
 
